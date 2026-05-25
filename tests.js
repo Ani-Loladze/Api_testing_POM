87935cd934e0a7e2b1428413dd2946d7fc8d22f7
Ani-Loladze <ani_loladze@epam.com>
2026-05-25 11:37:45 +0400

    Update tests.js

--- tests.js
+++ tests.js
@@ -12,14 +12,14 @@ console.log('TEST 1 HAS PASSED - Login successful');
  
 //TEST 2 - Create the booking
 const createRes = await bookingPage.create(BookingFactory.create());
-console.assert(createRes.status === 200,                    'Create booking failed');
+console.assert(createRes.status === 200,  'Create booking failed');
 console.assert(createRes.body.booking.firstname === 'Jane', 'Wrong firstname');
 bookingId = createRes.body.bookingid;
 console.log('TEST 2 HAS  PASSED - Create one booking');
  
 //TEST 3 - Get booking
 const getRes = await bookingPage.get(bookingId);
-console.assert(getRes.status === 200,            'Get booking has failed');
+console.assert(getRes.status === 200,  'Get booking has failed');
 console.assert(getRes.body.firstname === 'Jane', 'Wrong firstname');
 console.log('TEST 3 HAS PASSED - Get the booking');
  
